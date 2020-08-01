ec6f66d7731ac748171696b68906b09b7529febf
tthomasreid <61207807+tthomasreid@users.noreply.github.com>
2020-08-01 16:47:45 +0100

    bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.VYtsnWZZdvmm330QA5M0kodKF7fz8olB);//BOT_TOKEN is the Client Secret
